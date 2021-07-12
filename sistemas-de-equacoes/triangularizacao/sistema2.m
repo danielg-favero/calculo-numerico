2a980bed0588d2c1567f0180cb46697b7f920ac9
Proyol <danielfavero17@gmail.com>
2021-07-12 19:37:16 -0300

    added gauss's method for systems of linear equations exercises

---
+++ sistemas-de-equacoes/triangularizacao/sistema2.m
@@ -0,0 +1,146 @@
+clc
+clear
+
+% para n = 5, 10, 20, 50
+n1 = 5;
+n2 = 10;
+n3 = 20;
+n4 = 50;
+
+%n = 5
+
+% iniciar a matriz dos coeficientes e o vetor de soluções
+for i = 1: n1
+  for j = 1: n1
+     A(i, j) = 1 / (i + j - 1);
+  endfor
+  b(i) = 1 / i;
+endfor
+
+for k = 1: n1 - 1
+  for i = k + 1: n1
+    m(i, k) = A(i, k) / A(k, k);
+    A(i, k) = 0;
+    
+    for j = k + 1: n1
+      A(i, j) = A(i, j) - m(i, k) * A(k, j);
+    endfor
+    
+    b(i) = b(i) - m(i, k) * b(k); 
+  endfor
+endfor
+
+for i = n1: -1: 1
+  sum = b(i);
+  
+  for k = i + 1: n1
+    sum = sum - A(i, k) * x1(k);
+  endfor
+  
+  x1(i) = sum / A(i, i);
+endfor
+
+
+% n = 10
+
+for i = 1: n2
+  for j = 1: n2
+     A(i, j) = 1 / (i + j - 1);
+  endfor
+  b(i) = 1 / i;
+endfor
+
+for k = 1: n2 - 1
+  for i = k + 1: n2
+    m(i, k) = A(i, k) / A(k, k);
+    A(i, k) = 0;
+    
+    for j = k + 1: n2
+      A(i, j) = A(i, j) - m(i, k) * A(k, j);
+    endfor
+    
+    b(i) = b(i) - m(i, k) * b(k); 
+  endfor
+endfor
+
+for i = n2: -1: 1
+  sum = b(i);
+  
+  for k = i + 1: n2
+    sum = sum - A(i, k) * x2(k);
+  endfor
+  
+  x2(i) = sum / A(i, i);
+endfor
+
+
+% n = 20
+
+for i = 1: n3
+  for j = 1: n3
+     A(i, j) = 1 / (i + j - 1);
+  endfor
+  b(i) = 1 / i;
+endfor
+
+for k = 1: n3 - 1
+  for i = k + 1: n3
+    m(i, k) = A(i, k) / A(k, k);
+    A(i, k) = 0;
+    
+    for j = k + 1: n3
+      A(i, j) = A(i, j) - m(i, k) * A(k, j);
+    endfor
+    
+    b(i) = b(i) - m(i, k) * b(k); 
+  endfor
+endfor
+
+for i = n3: -1: 1
+  sum = b(i);
+  
+  for k = i + 1: n3
+    sum = sum - A(i, k) * x3(k);
+  endfor
+  
+  x3(i) = sum / A(i, i);
+endfor
+
+
+% n = 50
+
+for i = 1: n4
+  for j = 1: n4
+     A(i, j) = 1 / (i + j - 1);
+  endfor
+  b(i) = 1 / i;
+endfor
+
+for k = 1: n4 - 1
+  for i = k + 1: n4
+    m(i, k) = A(i, k) / A(k, k);
+    A(i, k) = 0;
+    
+    for j = k + 1: n4
+      A(i, j) = A(i, j) - m(i, k) * A(k, j);
+    endfor
+    
+    b(i) = b(i) - m(i, k) * b(k); 
+  endfor
+endfor
+
+for i = n4: -1: 1
+  sum = b(i);
+  
+  for k = i + 1: n4
+    sum = sum - A(i, k) * x4(k);
+  endfor
+  
+  x4(i) = sum / A(i, i);
+endfor
+  
+
+x1
+x2
+x3
+x4
---
+++ sistemas-de-equacoes/triangularizacao/−2(1 + h²)x1 + x2 = 1.m
@@ -0,0 +1,73 @@
+clc
+clear
+
+% valor de h
+h = 0.1;
+
+% número de iterações
+n = 5;
+
+% definição das equações
+b(1) = 1;
+
+for i = 1: n
+  for j = 1: n
+    A(i, j) = 0;
+  endfor
+  
+  if i > 1
+    A(i, i - 1) = 1;
+    b(i) = 0;   
+  endif
+  
+   A(i, i) = -2 * (1 + h^2);
+   
+   if i < n - 1
+     A(i, i + 1) = 1;   
+   endif 
+endfor
+
+% primeira equação
+A(1, 1) = -2 * (1 + h^2);
+A(1, 2) = 1;
+
+% última equação
+A(n, n-1) = 1;
+A(n, n) = -2 * (1 + h^2);
+
+b(n) = 1;
+
+A
+b
+
+% resolução do sistema
+for k = 1: n - 1
+  for i = k + 1: n
+    
+    % zerar os elementos abaixo da diagonal principal
+     m(i, k) = A(i, k) / A(k, k);
+     A(i, k) = 0;
+   
+      for j = k + 1: n
+        % realizar as operações de alteração da linha
+        A(i, j) = A(i, j) - m(i, k) * A(k, j);
+      endfor
+      
+      % alterar o vetor de soluções da
+      b(i) = b(i) - m(i, k) * b(k);
+  endfor
+endfor
+
+% substituição regressiva dos valores
+% inicia-se na última posição pois é aquela que temos a solução de cara
+for i = n: -1: 1
+  sum = b(i);
+  
+  for k = i + 1: n
+    sum = sum - A(i, k) * x(k);
+  endfor
+  
+  x(i) = sum / A(i, i);
+endfor
+
+x
